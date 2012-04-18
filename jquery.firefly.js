0a9959d84c9369f62c21dfbb77504d3a03734498
Matt Stevens <matt@dirtymonkey.co.uk>
2012-04-18 15:40:20 -0400

    Remove semicolons, they're optional! http://goo.gl/xqVHw

--- jquery.firefly.js
+++ jquery.firefly.js
@@ -1,14 +1,14 @@
-﻿/*
+/*
  * FireFly v1.0 - jQuery plugin
  *
  * An updated version of Dharmveer Motyar's firefly plugin.
  * Creates an effect of fireflys flying around your page.
  *
- * @example  $.firefly();
+ * @example  $.firefly()
  *
  *           // to stop and start
- *           $.firefly.stop();
- *           $.firefly.start();
+ *           $.firefly.stop()
+ *           $.firefly.start()
  *
  * Copyright © 2011 Matt 'Dirty Monkey' Stevens, http://www.dirtymonkey.co.uk
  * License: http://www.opensource.org/licenses/mit-license.php
@@ -70,71 +70,71 @@
         fast:     5,    // fastest spark (a lower number is faster)
         slow:     1,    // slowest spark (a higher number is slower)
         limit:    3600, // stop after this many seconds (one hour)
-    };
+    }
 
 
     // shortcuts
-    var ff, ffs, x, y, start, timer_handle, sparks = [];
+    var ff, ffs, x, y, start, timer_handle, sparks = []
 
 
     // init
     // initializes and starts everything
     $.firefly = function(settings){
-        ff = $.firefly;
-        ffs = $.extend({}, defaults, settings);
+        ff = $.firefly
+        ffs = $.extend({}, defaults, settings)
 
-        ff.calibrate();  // get size of window
-        ff.make();       // load the spark images
-        ff.start();      // start moving them
-        ff.check_time(); // start timer
-    };
+        ff.calibrate()  // get size of window
+        ff.make()       // load the spark images
+        ff.start()      // start moving them
+        ff.check_time() // start timer
+    }
 
 
     // create
     // creates every spark and inserts it into the DOM
     $.firefly.make = function(){
         for (i = 0; i < ffs.total; i++)
-            sparks[i] = ff.create(ffs.images[ff.random(0,ffs.images.length-1)]);
-    };
+            sparks[i] = ff.create(ffs.images[ff.random(0,ffs.images.length-1)])
+    }
 
 
     // start
     // records start time, watches window resize and begins movement
     $.firefly.start = function(){
-        start = parseInt((new Date).getTime()/1000);
-        $(window).bind('resize.firefly',function(){ff.calibrate()});
-        for (i = 0; i < sparks.length; i++) ff.move(sparks[i]);
-    };
+        start = parseInt((new Date).getTime()/1000)
+        $(window).bind('resize.firefly',function(){ff.calibrate()})
+        for (i = 0; i < sparks.length; i++) ff.move(sparks[i])
+    }
 
 
     // stop
     // freezes a spark in its current position
     $.firefly.stop = function(){
-        $(window).unbind('resize.firefly');
-        for (i = 0; i < sparks.length; i++) $(sparks[i]).stop(true);
-    };
+        $(window).unbind('resize.firefly')
+        for (i = 0; i < sparks.length; i++) $(sparks[i]).stop(true)
+    }
 
 
     // remove
     // removes a spark from the DOM
     $.firefly.remove = function() {
-        for (i = 0; i < ffs.total; i++) $('img[src="'+ffs.images[i]+'"]').remove();
-    };
+        for (i = 0; i < ffs.total; i++) $('img[src="'+ffs.images[i]+'"]').remove()
+    }
 
     // create
     // inserts a spark into the DOM at a random position
     $.firefly.create = function(i){
-        var spark = $('<img>').attr({'src':i}).hide();
-        $("body").append(spark);
+        var spark = $('<img>').attr({'src':i}).hide()
+        $("body").append(spark)
 
         spark.css({'position': 'absolute',
                     'z-index':  ff.random(0, 20),
                     'top':      ff.random(ffs.boundary, y - ffs.boundary),
                     'left':     ff.random(ffs.boundary, x - ffs.boundary)
-                    });
+                    })
 
-        return spark.show();
-    };
+        return spark.show()
+    }
 
 
     // move
@@ -143,32 +143,32 @@
         image.animate({top:  ff.random(ffs.boundary,y-ffs.boundary),  // x coordinate
                        left: ff.random(ffs.boundary,x-ffs.boundary)}, // y coordinate
                        ff.random(ffs.slow,ffs.fast) * 1000,           // speed
-                       function(){ff.move(image)});                   // callback
-    };
+                       function(){ff.move(image)})                    // callback
+    }
 
 
     // check_time
     // check every second whether or not we have passed the time limit
     $.firefly.check_time = function(){
         if (parseInt((new Date).getTime()/1000) > (start+ffs.limit)) {
-            ff.stop();
+            ff.stop()
         } else {
-            window.setTimeout('$.firefly.check_time()', 1000);
+            window.setTimeout('$.firefly.check_time()', 1000)
         }
-    };
+    }
 
 
     // calibrate
     // get the dimensions of the window
     $.firefly.calibrate = function(){
-        x = $(window).width();
-        y = $(window).height();
-    };
+        x = $(window).width()
+        y = $(window).height()
+    }
 
 
     // random
     // return a random number between a and b
     $.firefly.random = function(a,b){
-        return Math.floor(Math.random()*(b-a+1)+a);
-    };
-})(jQuery);
+        return Math.floor(Math.random()*(b-a+1)+a)
+    }
+})(jQuery)
